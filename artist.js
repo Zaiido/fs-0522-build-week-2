8270f74db14347e67433869eb2aa9c67fd08efab
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2023-01-24 15:47:50 +0100

    first comit

---
+++ artist.js
@@ -0,0 +1,53 @@
+const link = " https://striveschool-api.herokuapp.com/api/deezer/artist/412";
+const linksongs =
+  "https://striveschool-api.herokuapp.com/api/deezer/search?q=queen";
+
+const getDataArtist = () => {
+  fetch(link)
+    .then((dataRaw) => dataRaw.json())
+    .then((data) => {
+      console.log(data);
+      renderArtist(data);
+    });
+};
+
+const getDataSongs = () => {
+  fetch(linksongs)
+    .then((dataRaw) => dataRaw.json())
+    .then((data) => {
+      console.log(data);
+      renderSongs(data.data);
+    });
+};
+
+const renderArtist = async (artist) => {
+  const containerBg = document.getElementById("image-wrapper");
+  containerBg.innerHTML = "";
+  containerBg.innerHTML += `  
+            <img src="${artist.picture_big}" alt="">
+  `;
+  const artistPickImg = document.getElementById("album-photo");
+  artistPickImg.innerHTML = "";
+  artistPickImg.innerHTML += `
+  <img src="${artist.picture}" alt="">`;
+};
+
+const renderSongs = async (songs) => {
+  let containerList = document.getElementById("artist-list-songs");
+  containerList.innerHTML = "";
+  for (const iterator of songs) {
+    containerList.innerHTML += `
+    <li class="d-flex justify-content-between"><span> <img class="p-1" src="${iterator.album.cover}" height="48" width="48" alt="">
+    ${iterator.album.title}</span><span class="grey">${iterator.duration}s</span></li>`;
+  }
+};
+//   await songs.forEach((singleSong) => {
+//     containerList.innerHTML += `
+//       <li class="d-flex justify-content-between"><span> <img class="p-1" src="${singleSong.album.cover}" height="48" width="48" alt="">
+//       ${singleSong.album.title}</span><span class="grey">1.013,238.772</span><span class="grey">${singleSong.duration}</span></li>`;
+//   });
+
+window.onload = () => {
+  getDataArtist();
+  getDataSongs();
+};
