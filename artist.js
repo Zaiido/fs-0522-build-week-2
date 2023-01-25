7beb74063d4688ea6ba9c86b17c577135fc5ac24
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2023-01-25 08:58:49 +0100

    Deleted some lines

--- artist.js
+++ artist.js
@@ -1,21 +1,22 @@
-const link = " https://striveschool-api.herokuapp.com/api/deezer/artist/412";
+// const link = " https://striveschool-api.herokuapp.com/api/deezer/artist/412";
 const linksongs =
   "https://striveschool-api.herokuapp.com/api/deezer/search?q=queen";
 
-const getDataArtist = () => {
-  fetch(link)
-    .then((dataRaw) => dataRaw.json())
-    .then((data) => {
-      console.log(data);
-      renderArtist(data);
-    });
-};
+// const getDataArtist = () => {
+//   fetch(link)
+//     .then((dataRaw) => dataRaw.json())
+//     .then((data) => {
+//       console.log(data);
+//       renderArtist(data);
+//     });
+// };
 
 const getDataSongs = () => {
   fetch(linksongs)
     .then((dataRaw) => dataRaw.json())
     .then((data) => {
       console.log(data);
+      renderArtist(data.data[0]);
       renderSongs(data.data);
     });
 };
@@ -24,16 +25,16 @@ const renderArtist = async (artist) => {
   const containerBg = document.getElementById("image-wrapper");
   containerBg.innerHTML = "";
   containerBg.innerHTML += `  
-            <img src="${artist.picture_big}" alt="">
+            <img src="${artist.artist.picture_big}" alt="">
   `;
   const artistName = document.getElementById("artist-name");
-  artistName.innerText = `${artist.name}`;
+  artistName.innerText = `${artist.artist.name}`;
   const artistNameSmall = document.getElementById("artist-by-name");
-  artistNameSmall.innerText = `${artist.name} Best of`;
+  artistNameSmall.innerText = `${artist.artist.name} Best of`;
   const artistPickImg = document.getElementById("album-photo");
   artistPickImg.innerHTML = "";
   artistPickImg.innerHTML += `
-  <img src="${artist.picture}" alt="">`;
+  <img src="${artist.artist.picture}" alt="">`;
 };
 
 const renderSongs = async (songs) => {
@@ -49,14 +50,9 @@ const renderSongs = async (songs) => {
     )}m${iterator.duration % 60}s</span></li>`;
   }
 };
-//   await songs.forEach((singleSong) => {
-//     containerList.innerHTML += `
-//       <li class="d-flex justify-content-between"><span> <img class="p-1" src="${singleSong.album.cover}" height="48" width="48" alt="">
-//       ${singleSong.album.title}</span><span class="grey">1.013,238.772</span><span class="grey">${singleSong.duration}</span></li>`;
-//   });
 
 window.onload = () => {
-  getDataArtist();
+  // getDataArtist();
   getDataSongs();
 };
 
@@ -64,8 +60,6 @@ const imgChange = function () {
   const img = document.querySelector(".image-wrapper img");
   const imgContainer = document.querySelector(".image-wrapper");
   if (window.pageYOffset > 300) {
-    console.log("it went there");
-    console.log(img);
     img.classList.add("opacity");
     imgContainer.style.backgroundColor = "#525252";
   } else if (window.pageYOffset < 300) {
