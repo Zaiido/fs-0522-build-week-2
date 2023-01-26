41b4fabd8f922874a970054e40c1543ca58b7d49
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2023-01-26 08:34:43 +0100

    added right query links,  trying to make songs be active on click

--- artist.js
+++ artist.js
@@ -1,7 +1,7 @@
 // const link = " https://striveschool-api.herokuapp.com/api/deezer/artist/412";
-const linksongs =
-  "https://striveschool-api.herokuapp.com/api/deezer/search?q=queen";
-
+const linksongs = "https://striveschool-api.herokuapp.com/api/deezer/search?q=";
+const params = new URLSearchParams(location.search);
+const id = params.get("id");
 // const getDataArtist = () => {
 //   fetch(link)
 //     .then((dataRaw) => dataRaw.json())
@@ -12,7 +12,7 @@ const linksongs =
 // };
 
 const getDataSongs = () => {
-  fetch(linksongs)
+  fetch(linksongs + id)
     .then((dataRaw) => dataRaw.json())
     .then((data) => {
       console.log(data);
@@ -55,42 +55,14 @@ const renderSongs = async (songs) => {
     } = await iterator;
     // updatePlayerDuration("${duration}")
     containerList.innerHTML += await ` 
-    <tr onclick='player("${preview}"); updatePlayerCover("${cover}"); updatePlayerName("${title}"); updatePlayerArtist("${name}")')>
-    <th scope="row" class="grey">${i + 1}</th>
-    <td>
-    <img class="p-1" src="${cover}" height="48" width="48" alt="">
-    </td>
-    <td><span class="grey" )>
-      ${title}
-    </span></td>
-    <td><span class="grey"><a href="/album-page.html?id=${
-      iterator.album.id
-    }"> @ ${iterator.album.title}</a></span></td>
-    <td><span class="grey">${parseInt(duration / 60)}m${
-      duration % 60
-    }s</span></td>
-  </tr>`;
-  }
-  document.getElementById("see-more").addEventListener("click", function () {
-    renderMoreSongs(songs);
-    document.getElementById("see-more").remove();
-  });
-};
-
-const renderMoreSongs = (songs) => {
-  console.log(songs);
-  let containerList = document.getElementById("tbody");
-  for (let i = 5; i < songs.length; i++) {
-    const iterator = songs[i];
-    containerList.innerHTML += `
-    <tr onclick='player("${iterator.preview}")')>
-    <th scope="row" class="grey">${i + 1}</th>
+    <tr onclick='player("${preview}"); updatePlayerCover("${cover}"); updatePlayerName("${title}"); updatePlayerArtist("${name}"); makeGreen(); makeChange(event)')>
+    <th scope="row" class="grey row-btn">${i + 1}</th>
     <td>
     <img class="p-1" src="${
       iterator.album.cover
     }" height="48" width="48" alt="">
     </td>
-    <td><span class="grey">
+    <td><span class="grey song-btn" id="title">
       ${iterator.title}
     </span></td>
     <td><span class="grey"><a href="/album-page.html?id=${
@@ -100,6 +72,45 @@ const renderMoreSongs = (songs) => {
       iterator.duration % 60
     }s</span></td>
   </tr>`;
+  }
+  document.getElementById("see-more").addEventListener("click", function () {
+    renderMoreSongs(songs);
+    document.getElementById("see-more").remove();
+  });
+};
+
+const renderMoreSongs = async (songs) => {
+  console.log(songs);
+  let containerList = document.getElementById("tbody");
+  for (let i = 5; i < songs.length; i++) {
+    const iterator = songs[i];
+    const {
+      album: { cover },
+      artist: { name },
+      title,
+      duration,
+      preview,
+    } = await iterator;
+
+    containerList.innerHTML += `
+    <tr onclick='player("${preview}"); updatePlayerCover("${cover}"); updatePlayerName("${title}"); updatePlayerArtist("${name}"); makeGreen(); makeChange(event)')>
+    <th scope="row" class="grey row-btn">${i + 1}</th>
+    <td>
+    <img class="p-1" src="${
+      iterator.album.cover
+    }" height="48" width="48" alt="">
+    </td>
+    <td><span class="grey song-btn" id="title">
+      ${iterator.title}
+    </span></td>
+    <td><span class="grey"><a href="/album-page.html?id=${
+      iterator.album.id
+    }"> @ ${iterator.album.title}</a></span></td>
+    <td><span class="grey">${parseInt(iterator.duration / 60)}m${
+      iterator.duration % 60
+    }s</span></td>
+  </tr>
+`;
   }
 };
 
@@ -162,6 +173,7 @@ const player = (song) => {
   const stopContainer = document.getElementById("stop");
 
   PlayIconContainer.addEventListener("click", ifPlay());
+  stopContainer.addEventListener("click", ifPlay());
 };
 
 const updatePlayerCover = (cover) => {
@@ -179,12 +191,28 @@ const updatePlayerArtist = (artist) => {
   const container = document.getElementById("player-artist");
   container.innerText = artist;
 };
-// const updatePlayerDuration = (duration) => {
-//   const container = document.getElementById("player-time");
-//   const minutes = parseInt(duration / 60);
-//   const seconds = parseFloat(duration % 60);
-//   container.innerText = `${minutes}:${seconds}`;
-// };
+
+const makeGreen = () => {
+  let header = document.getElementById("tbody");
+  let btns = header.getElementsByClassName("song-btn");
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+      let current = document.getElementsByClassName("active-text");
+      if (current.length > 0) {
+        current[0].className = current[0].className.replace(" active-text", "");
+      }
+      this.className += " active-text";
+    });
+  }
+};
+
+const makeChange = (event) => {
+  const rowTo = event.target.parentElement.parentElement.firstElementChild;
+  console.log(event.target.parentElement.parentElement.firstElementChild);
+  // const secondrow = rowTo.nextSibling.nextSibling.nextSibling.nextSibling;
+
+  rowTo.innerHTML = `<i class="bi bi-play-fill"></i>`;
+};
 
 window.addEventListener("scroll", imgChange);
 window.onload = () => {
