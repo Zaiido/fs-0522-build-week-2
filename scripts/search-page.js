bba34e888aa6128f249cc428dc3f4b477480d12f
Eleni-Ke <97251568+Eleni-Ke@users.noreply.github.com>
2023-01-26 11:38:06 +0100

    play buttons added

--- scripts/search-page.js
+++ scripts/search-page.js
@@ -34,6 +34,15 @@ const displaySongs = (arr) => {
             ><img class="card-img-top" src="${song.album.cover}" alt="song cover"
             /></a>
             <div class="card-body">
+            <button class="btn play-button ml-auto mr-3">
+              <svg role="img" height="24" width="24" aria-hidden="true"
+              viewBox="0 0 24 24" data-encore-id="icon"
+              class="Svg-sc-ytk21e-0 uPxdw">
+              <path
+                d="M7.05 3.606l13.49 7.788a.7.7 0 010 1.212L7.05 20.394A.7.7 0 016 19.788V4.212a.7.7 0 011.05-.606z">
+              </path>
+              </svg>
+            </button>
             <a href="./album-page.html?id=${song.album.id}"><h5 class="card-title truncate">${song.album.title}</h5></a>
             <a href="./artist.html?name=${song.artist.name}"><p class="card-text truncate">${song.artist.name}</p></a>
             </div>
